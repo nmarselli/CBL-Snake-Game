5cd9e30ef619b81fed2ecb3c567ad3b6590b4c3a
5Peter1 <p.skoda@student.tue.nl>
2025-10-11 14:10:04 +0200

    Co-authored-by: Nikolas Marselli <n.marselli@student.tue.nl>

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,5 @@
-
 import java.awt.*;
+import java.io.File;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
@@ -12,29 +12,50 @@ public class GameFrame extends JFrame {
     GameFrame() {
         // 1. Frame title and icon
         setTitle("Snake");
-        ImageIcon image = new ImageIcon("C:\\Users\\nmars\\OneDrive - TU Eindhoven\\" 
-                + "Desktop\\Snake Game\\assets\\images\\FrameIcon.jpg");
+        ImageIcon image = new ImageIcon("assets/images/FrameIcon.jpg");
         setIconImage(image.getImage());
-
         // 2. Frame basic properties
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setSize(500, 600);        // size first
-        setLocationRelativeTo(null);         // then center on screen
-        getContentPane().setBackground(new Color(0, 255, 0)); // background color
+        setSize(400, 600); // size first
+        setLocationRelativeTo(null); // then center on screen
+        getContentPane().setBackground(Color.WHITE); // background color
 
         // 3. Layout manager
-        setLayout(null); 
+        setLayout(null);
 
         // 4. Build UI components
-        Settings();
-
-        //Make frame visible
+        settings();
+        // Make frame visible
         setVisible(true);
     }
 
-    public void Settings() {
-    
+    private void settings() {
+        JPanel settingsPanel = new JPanel();
+
+        settingsPanel.setBackground(Color.blue);
+        this.add(settingsPanel);
+        settingsPanel.setLayout(null);
+        settingsPanel.setBounds(0, 0, 400, 600);
+        
+        ImageIcon ArrowLeftBlack = new ImageIcon("assets/images/ArrowLeftBlack.jpg");
+        for (int i = 0; i < leftButtons.length; i++) {
+            leftButtons[i] = new JButton();
+            leftButtons[i].setIcon(ArrowLeftBlack);
+            leftButtons[i].setBorder(BorderFactory.createEmptyBorder());
+            leftButtons[i].setContentAreaFilled(false);
+            leftButtons[i].setBounds(30, 30 + i * 75, 50, 50);
+
+            settingsPanel.add(leftButtons[i]);
+
+            /*
+             * leftButtons[i].addActionListener(new ActionListener() {
+             * 
+             * @Override
+             * public void actionPerformed(ActionEvent e) {
+             * //your actions
+             */
+        }
     }
 
 }
